c9b134114848c34d3a7f3ae16024cb7f7d6ba778
Fahad Islam <encyclogames@encyclo-mbp.local>
2011-07-21 15:55:14 +0300

    Initial Commit

---
+++ TwitterSearch/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TwitterSearch
+//
+//  Created by Fahad Islam on 7/21/11.
+//  Copyright 2011 Carnegie Mellon Qatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ TwitterSearch/TwitterSearchAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TwitterSearchAppDelegate.h
+//  TwitterSearch
+//
+//  Created by Fahad Islam on 7/21/11.
+//  Copyright 2011 Carnegie Mellon Qatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterSearchAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
